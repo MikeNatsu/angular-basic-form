ffb092903eea929c13281ad873f12caada0a3f11
MikeNatsu <michaelgonzalezsantamaria@gmail.com>
2021-07-16 20:12:10 -0400

    feature: adding sidebar function

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Subscription } from 'rxjs';
+import { UiService } from '../../services/ui.service';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  showSidebar: boolean = false;
+  subscription: Subscription;
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => {
+      this.showSidebar = value;
+    });
   }
 
+  ngOnInit(): void {}
 }
