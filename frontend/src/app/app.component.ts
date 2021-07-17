41431e2cfec2f4d37e7e31a892e0b6802eb83976
MikeNatsu <michaelgonzalezsantamaria@gmail.com>
2021-07-17 19:52:59 -0400

    style fixed: making it responsive :)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private router: Router) {}
+  hasRoute(router: string) {
+    return router === this.router.url;
+  }
 }
